604c858c17e51659f64761104f1f655092979027
Anthony Rod <arods.island.2@gmail.com>
2022-04-29 21:44:52 +0000

    Added flow that take user to the create profile page upon creating a new user

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,9 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  
+protected
+
+  def after_sign_up_path_for(resource)
+    new_user_profile_path(user_id: current_user.id)
+  end
+  
+end
